1ea7f29e314d961b35b68ebef2b5936f5921a094
Grace Chen <gchen20@stuy.edu>
2022-01-06 03:39:44 -0500

    cleaned up

--- pipe_networking.c
+++ pipe_networking.c
@@ -1,14 +1,5 @@
 #include "pipe_networking.h"
 
-
-/*=========================
-  server_setup
-  args:
-  creates the WKP (upstream) and opens it, waiting for a
-  connection.
-  removes the WKP once a connection has been made
-  returns the file descriptor for the upstream pipe.
-  =========================*/
 int server_setup() {
   struct addrinfo * hints, * results;
   hints = calloc(1,sizeof(struct addrinfo));
@@ -30,12 +21,6 @@ int server_setup() {
   return sd;
 }
 
-/*=========================
-  server_connect
-  args: int from_client
-  handles the subserver portion of the 3 way handshake
-  returns the file descriptor for the downstream pipe.
-  =========================*/
 int server_connect(int sd) {
   int client_socket;
   socklen_t sock_size;
@@ -45,62 +30,6 @@ int server_connect(int sd) {
   return client_socket;
 }
 
-
-/*=========================
-  server_handshake
-  args: int * to_client
-  Performs the server side pipe 3 way handshake.
-  Sets *to_client to the file descriptor to the downstream pipe.
-  returns the file descriptor for the upstream pipe.
-  =========================*/
-int server_handshake(int *to_client) {
-  int b, from_client;
-  char buffer[HANDSHAKE_BUFFER_SIZE];
-
-  printf("[server] handshake: making wkp\n");
-  b = mkfifo(WKP, 0600);
-  if ( b == -1 ) {
-    printf("mkfifo error %d: %s\n", errno, strerror(errno));
-    exit(-1);
-  }
-  //open & block
-  from_client = open(WKP, O_RDONLY, 0);
-  //remove WKP
-  remove(WKP);
-
-  printf("[server] handshake: removed wkp\n");
-  //read initial message
-  b = read(from_client, buffer, sizeof(buffer));
-  printf("[server] handshake received: -%s-\n", buffer);
-
-
-  *to_client = open(buffer, O_WRONLY, 0);
-  //create SYN_ACK message
-  srand(time(NULL));
-  int r = rand() % HANDSHAKE_BUFFER_SIZE;
-  sprintf(buffer, "%d", r);
-
-  write(*to_client, buffer, sizeof(buffer));
-  //rad and check ACK
-  read(from_client, buffer, sizeof(buffer));
-  int ra = atoi(buffer);
-  if (ra != r+1) {
-    printf("[server] handshake received bad ACK: -%s-\n", buffer);
-    exit(0);
-  }//bad response
-  printf("[server] handshake received: -%s-\n", buffer);
-
-  return from_client;
-}
-
-
-/*=========================
-  client_handshake
-  args: int * to_server
-  Performs the client side pipe 3 way handshake.
-  Sets *to_server to the file descriptor for the upstream pipe.
-  returns the file descriptor for the downstream pipe.
-  =========================*/
 int client_handshake() {
   struct addrinfo * hints, * results;
   hints = calloc(1,sizeof(struct addrinfo));
@@ -112,6 +41,6 @@ int client_handshake() {
   //create socket
   int sd = socket(results->ai_family, results->ai_socktype, results->ai_protocol);
   connect(sd, results->ai_addr, results->ai_addrlen);
-  printf("[client] connected\n");
+
   return sd;
 }
